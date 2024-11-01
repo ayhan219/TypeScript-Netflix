4f1aac3278c4d0b5521555f28d3fe0acc0c3f639
ayhan219 <ayhan-games@hotmail.com>
2024-11-01 12:10:16 +0300

    movies,tvshows page and moviesSingle updated

---
+++ client/src/components/MoviesSingle.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+type MoviesProps ={
+    item:{
+        poster_path:string,
+        vote_average:number,
+        title:string
+    }
+    index:number
+}
+
+const MoviesSingle = ({item,index}:MoviesProps) => {
+    const show =()=>{
+        console.log(item.title);
+        
+    }
+ return(
+    <div onClick={show} key={index} className='border-2 border-gray-800 w-72 h-96 solid'>
+                    <div>
+                        <img className='object-cover object-center w-full h-52' src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} alt="" />
+                    </div>
+                    <div className='w-full h-auto text-base font-semibold text-white'>
+                        <div className='flex justify-between w-full p-3'>
+                            <h3>Name:</h3>
+                            <span className='text-base'>{item.title}</span>
+                        </div>
+                        <div className='flex justify-between w-full p-3'>
+                            <h3>IMDB:</h3>
+                            <span>{item.vote_average}</span>
+                        </div>
+                    </div>
+                </div>
+ )
+}
+
+export default MoviesSingle
