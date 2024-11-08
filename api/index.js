575fa26389fd58291c255471f6c56289a793ad8b
ayhan219 <ayhan-games@hotmail.com>
2024-11-08 22:30:02 +0300

    index.js updated cors added

--- api/index.js
+++ api/index.js
@@ -3,10 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const AuthRouter = require("./router/UserRouter")
+const cors = require("cors");
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth",AuthRouter);
+
 
 const connectToDB = async () => {
   try {
