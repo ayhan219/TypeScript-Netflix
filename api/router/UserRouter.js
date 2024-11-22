4592bd84eebc53291007a29f0636bae455ac4c00
Ayhan <ayhan-games@hotmail.com>
2024-11-23 00:53:33 +0300

    userrouter updated

--- api/router/UserRouter.js
+++ api/router/UserRouter.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const {signUp,login,getCurrentUser} = require("../controller/UserController")
+const {signUp,login,getCurrentUser,logoutUser} = require("../controller/UserController")
 const AuthMiddleware = require("../middleware/AuthMiddleware")
 const router = express.Router();
 
 router.post("/signup",signUp);
 router.post("/login",login)
 router.get("/getcurrent",AuthMiddleware,getCurrentUser)
+router.delete("/logout",logoutUser)
 
 
 module.exports = router;
