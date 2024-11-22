0949acf6dea490076be11afce7c057319e25689d
Ayhan <ayhan-games@hotmail.com>
2024-11-23 02:04:13 +0300

    model updated

--- api/model/User.js
+++ api/model/User.js
@@ -20,7 +20,14 @@ const UserSchema = mongoose.Schema({
         type: Date,
         default: Date.now,
       },
-
+      favorites: [
+        {
+          id: { type: String, required: true }, 
+          type: { type: String, enum: ["movie", "tv"], required: true }, 
+          title: { type: String, required: true },
+          poster_path: { type: String }, 
+        },
+      ],
 });
 
 module.exports = mongoose.model("User",UserSchema);
